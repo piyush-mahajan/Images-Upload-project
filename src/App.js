4b4d74e3f93cc1040d5b35c45813d5eea36da0c1
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2023-04-14 12:18:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState("");
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "geekyimages");
+    setLoading(true);
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/demsh5eg7/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+
+    setImage(file.secure_url);
+    setLoading(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Upload Images to Cloudinary In React</h1>
+
+      <input
+        type="file"
+        name="file"
+        placeholder="upload file"
+        onChange={uploadImage}
+      />
+
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <img src={image} style={{ width: "300px" }} />
+      )}
     </div>
   );
 }
